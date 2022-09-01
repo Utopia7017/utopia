600c04537bec5598d5936649091a0265eda65cca
Alpha17-2 <alphaisgod1@gmail.com>
2022-09-02 00:52:41 +0530

    Radhe Radhe

---
+++ android/app/src/main/kotlin/com/example/utopia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.utopia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Utopia());
+}
+
+class Utopia extends StatelessWidget {
+  const Utopia({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
