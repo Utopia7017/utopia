188e91f1e87b20c0ad87394ed31cd83f4dc17a6c
Alpha17-2 <alphaisgod1@gmail.com>
2022-09-02 15:03:15 +0530

    import : dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
-import 'package:flutter/cupertino.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:logging/logging.dart';
+import 'package:utopia/view/screens/AppScreen/app_screen.dart';
 
-void main() {
+/*
+  Project Name : Utoppia - 
+  Project Authors : { Subhojeet Sahoo, Gurudev Singh , Vishal Mahato , Abhishek Kumar }
+  Creation Date : 2nd Sep, 2022
+  Project Aim : Building Blogging application for learning purpose.
+*/
+
+void main() async {
+  // logger configuration
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((record) {
+    debugPrint(
+        '${record.loggerName} -> ${record.level.name}: ${record.message}');
+  });
+  await dotenv.load();
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  Logger("Root").fine("Utopia Initialised");
   runApp(Utopia());
 }
 
@@ -10,6 +30,8 @@ class Utopia extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: AppScreen(),
+    );
   }
 }
