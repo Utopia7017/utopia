513b5b7baf67df9388649563222d0537d94ac6ce
Alpha17-2 <alphaisgod1@gmail.com>
2022-09-05 20:18:15 +0530

    automatically set current user

--- lib/view/screens/AppScreen/drawer.dart
+++ lib/view/screens/AppScreen/drawer.dart
@@ -7,6 +7,7 @@ import 'package:utopia/controlller/user_controller.dart';
 import 'package:utopia/enums/enums.dart';
 import 'package:utopia/utils/device_size.dart';
 import '../../../services/firebase/auth_services.dart';
+// import 'package:utopia/models/user_model.dart' as user;
 
 class CustomDrawer extends StatelessWidget {
   final Logger _logger = Logger('CustomDrawer');
@@ -35,7 +36,10 @@ class CustomDrawer extends StatelessWidget {
         padding: const EdgeInsets.only(left: 20, top: 25, right: 14),
         child: Consumer<UserController>(
           builder: (context, controller, child) {
-            
+            if (controller.profileStatus == ProfileStatus.nil) {
+              controller.setUser(FirebaseAuth.instance.currentUser!.uid);
+            }
+
             switch (controller.profileStatus) {
               case ProfileStatus.nil:
                 return Center(
@@ -54,123 +58,124 @@ class CustomDrawer extends StatelessWidget {
                       CircularProgressIndicator(color: authMaterialButtonColor),
                 );
               case ProfileStatus.fetched:
-               return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  CircleAvatar(
-                    radius: displayWidth(context) * 0.13,
-                    backgroundImage: const NetworkImage(
-                        'https://i.pinimg.com/564x/17/b0/8f/17b08fc3ad0e62df60e15ef557ec3fe1.jpg'),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  const Text(
-                    '@Subhojeet Sahoo',
-                    style: TextStyle(
-                        color: Colors.white, fontSize: 18, fontFamily: "Fira"),
-                  ),
-                  const SizedBox(height: 16),
-                  // Follower detail box
-                  Container(
-                    height: displayHeight(context) * 0.08,
-                    alignment: Alignment.center,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const [
-                            Text(
-                              '50',
-                              style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w400),
-                            ),
-                            SizedBox(
-                              height: 3,
-                            ),
-                            Text(
-                              'Followers',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const [
-                            Text(
-                              '158',
-                              style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w400),
-                            ),
-                            SizedBox(
-                              height: 3,
-                            ),
-                            Text(
-                              'Following',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      radius: displayWidth(context) * 0.13,
+                      backgroundImage: const NetworkImage(
+                          'https://i.pinimg.com/564x/17/b0/8f/17b08fc3ad0e62df60e15ef557ec3fe1.jpg'),
                     ),
-                  ),
-                  const SizedBox(height: 16),
-                  drawerTile(
-                      'Write new article',
-                      const Icon(
-                        Icons.edit,
-                        color: Colors.white70,
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      '@${controller.user!.name}',
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontFamily: "Fira"),
+                    ),
+                    const SizedBox(height: 16),
+                    // Follower detail box
+                    Container(
+                      height: displayHeight(context) * 0.08,
+                      alignment: Alignment.center,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                controller.user!.followers.length.toString(),
+                                style: const TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                              const SizedBox(
+                                height: 3,
+                              ),
+                              const Text(
+                                'Followers',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                controller.user!.following.length.toString(),
+                                style: const TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                              const SizedBox(
+                                height: 3,
+                              ),
+                              const Text(
+                                'Following',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
                       ),
-                      () => _logger.info("Add new article")),
-                  drawerTile(
-                      'Your articles',
-                      const Icon(
-                        Icons.book_sharp,
-                        color: Colors.white70,
-                      ),
-                      () => _logger.info("Your articles")),
-                  drawerTile(
-                      'Search articles',
-                      const Icon(
-                        Icons.search_rounded,
-                        color: Colors.white70,
-                      ),
-                      () => _logger.info("Search")),
-                  drawerTile(
-                      'Notifications',
-                      const Icon(
-                        Icons.notifications_none_rounded,
-                        color: Colors.white70,
-                      ),
-                      () => _logger.info("Notifications")),
-                  drawerTile(
-                      'Logout',
-                      const Icon(
-                        Icons.logout,
-                        color: Colors.white70,
-                      ), () async {
-                    final navigator = Navigator.of(context);
-                    await _auth.signOut();
-                    navigator.pushReplacementNamed('/auth');
-                  }),
-                ],
-              );
+                    ),
+                    const SizedBox(height: 16),
+                    drawerTile(
+                        'Write new article',
+                        const Icon(
+                          Icons.edit,
+                          color: Colors.white70,
+                        ),
+                        () => _logger.info("Add new article")),
+                    drawerTile(
+                        'Your articles',
+                        const Icon(
+                          Icons.book_sharp,
+                          color: Colors.white70,
+                        ),
+                        () => _logger.info("Your articles")),
+                    drawerTile(
+                        'Search articles',
+                        const Icon(
+                          Icons.search_rounded,
+                          color: Colors.white70,
+                        ),
+                        () => _logger.info("Search")),
+                    drawerTile(
+                        'Notifications',
+                        const Icon(
+                          Icons.notifications_none_rounded,
+                          color: Colors.white70,
+                        ),
+                        () => _logger.info("Notifications")),
+                    drawerTile(
+                        'Logout',
+                        const Icon(
+                          Icons.logout,
+                          color: Colors.white70,
+                        ), () async {
+                      final navigator = Navigator.of(context);
+                      await _auth.signOut();
+                      navigator.pushReplacementNamed('/auth');
+                    }),
+                  ],
+                );
             }
-           
           },
         ),
       ),
