787ce1df50c1042d72d4663b70f81171bdfa66f2
Alpha17-2 <alphaisgod1@gmail.com>
2022-09-05 15:39:22 +0530

    new controller created for users

---
+++ lib/controlller/user_controller.dart
@@ -0,0 +1,43 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:logging/logging.dart';
+import 'package:utopia/enums/enums.dart';
+import 'package:utopia/models/user_model.dart';
+import 'package:utopia/services/api/api_services.dart';
+
+class UserController with ChangeNotifier {
+  final Logger _logger = Logger("UserController");
+  final ApiServices _apiServices = ApiServices();
+  ProfileStatus profileStatus = ProfileStatus.nil;
+  User? user;
+
+  setUser(String userId) async {
+    profileStatus = ProfileStatus.loading;
+    notifyListeners();
+    final endUrl = 'users/$userId.json';
+    try {
+      final Response? response = await _apiServices.get(endUrl: endUrl);
+      if (response != null) {
+        user = User.fromJson(response.data);
+      }
+    } catch (error) {
+      _logger.shout(error.toString());
+    }
+    profileStatus = ProfileStatus.fetched;
+    notifyListeners();
+  }
+
+  createUser(User newUser) async {
+    try {
+      final Response? response = await _apiServices.put(
+          endUrl: 'users/${newUser.userId}.json', data: newUser.toJson());
+      if (response != null) {
+        user = newUser;
+      }
+    } catch (error) {
+      _logger.shout(error.toString());
+    }
+    profileStatus = ProfileStatus.fetched;
+    notifyListeners();
+  }
+}
