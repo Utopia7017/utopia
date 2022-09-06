378307deebcd1e61978d74acb2663547b1940b9a
Alpha17-2 <alphaisgod1@gmail.com>
2022-09-06 15:02:29 +0530

    floating action button to navigate new screen

--- lib/view/screens/ExploreScreen/explore_screen.dart
+++ lib/view/screens/ExploreScreen/explore_screen.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+import 'package:utopia/constants/color_constants.dart';
 import 'package:utopia/view/screens/ExploreScreen/components/article_category_tabs.dart';
 import 'package:utopia/view/screens/ExploreScreen/components/search_box.dart';
 
 class ExploreScreen extends StatelessWidget {
-  const ExploreScreen({Key? key}) : super(key: key);
+  ExploreScreen({Key? key}) : super(key: key);
+  final Logger _logger = Logger("ExploreScreen");
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: const EdgeInsets.only(left: 18.0, right: 18, top: 6),
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          SearchBox(),
-          SizedBox(
-            height: 14,
-          ),
-          ArticleCategoryTab()
-        ],
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _logger.info("Add New Article");
+        },
+        backgroundColor: authBackground,
+        child: const Icon(
+          Icons.edit,
+          color: Colors.white,
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.only(left: 18.0, right: 18, top: 6),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            SearchBox(),
+            SizedBox(
+              height: 14,
+            ),
+            ArticleCategoryTab()
+          ],
+        ),
       ),
     );
   }
