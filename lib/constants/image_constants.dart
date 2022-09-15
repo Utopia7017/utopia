7dfabcddc85d67321201b5771835fa28d1461a4c
Alpha17-2 <alphaisgod1@gmail.com>
2022-09-15 18:29:25 +0530

    added new images,icons and fonts

--- lib/constants/image_constants.dart
+++ lib/constants/image_constants.dart
@@ -5,3 +5,13 @@ const menuIcon = 'assets/icons/menu.png';
 const menuIconClose = 'assets/icons/menu_close.png';
 
 const searchIcon = 'assets/icons/search_icon.png';
+
+const defaultArticleImage = 'assets/images/default_article.jpg';
+
+const likePressedIcon = 'assets/icons/like_pressed.png';
+
+const likeNotPressedIcon = 'assets/icons/like_unpressed.png';
+
+const commentArticleIcon = 'assets/icons/comment_article.png';
+
+const saveArticleIcon = 'assets/icons/save_article.png';
