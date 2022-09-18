57007dd65447def9f6abcc8fd4e517cdbbb1dc87
Alpha17-2 <alphaisgod1@gmail.com>
2022-09-19 02:11:11 +0530

    return null if caught firebase exception

--- lib/services/firebase/storage_service.dart
+++ lib/services/firebase/storage_service.dart
@@ -3,18 +3,16 @@ import 'dart:math';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:logging/logging.dart';
 
-Future<String?> getImageUrl(File image,String location)async{
+Future<String?> getImageUrl(File image, String location) async {
   Logger logger = Logger("Get image url");
-  try{
+  try {
     final Reference storageReference =
-    FirebaseStorage.instance.ref().child(location);
+        FirebaseStorage.instance.ref().child(location);
     final UploadTask uploadTask = storageReference.putFile(image);
     final TaskSnapshot taskSnapshot = await uploadTask;
     final url = await taskSnapshot.ref.getDownloadURL();
     return url;
+  } on FirebaseException catch (e) {
+    return null;
   }
-  on FirebaseException catch(e){
-   logger.shout(e.message);
-  }
-  return null;
 }
