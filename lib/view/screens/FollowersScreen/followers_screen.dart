38cf2cc2f8e7804494863f0da406d9d05f7f2790
kaizer111 <mahatovishal2001@gmail.com>
2022-09-23 23:46:38 +0530

    Followers and Following in their resoective screen

--- lib/view/screens/FollowersScreen/followers_screen.dart
+++ lib/view/screens/FollowersScreen/followers_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:utopia/constants/color_constants.dart';
 import 'package:utopia/models/user_model.dart';
+import 'package:utopia/view/screens/ExploreScreen/components/search_box.dart';
 
 class FollowersScreen extends StatelessWidget {
   final User user;
@@ -13,6 +14,52 @@ class FollowersScreen extends StatelessWidget {
         backgroundColor: authBackground,
         title: const Text('Followers',style: TextStyle(fontFamily: "Fira",fontSize: 15),),
         iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 6),
+            child: SearchBox(),
+          ),
+          Expanded(
+            child: ListView.builder(
+                  padding: EdgeInsets.all(8),
+                  itemCount: 15,
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemBuilder: (BuildContext, context) 
+                  {
+                  return Container(
+                    height: 70,
+                    width: 100,
+                    color: Colors.white60,
+                    margin:  EdgeInsets.all(8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children:   [
+                       Padding(
+                          padding: EdgeInsets.only(left: 6),
+                          child: CircleAvatar(
+                            radius: 28,
+                            backgroundColor: Colors.amber,
+                          ),
+                        ),
+                        Text('Kakashi Hatake', style: TextStyle(fontWeight: FontWeight.w700,fontSize: 17.5),),
+                        MaterialButton(onPressed: () {
+                          
+                        },
+                        color: authMaterialButtonColor,
+                        shape:  RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(6),
+                               ),
+                          child: Text('Remove',style: TextStyle(fontSize: 15.5),),
+                        ),
+                      ],
+                    ),
+                  );
+                },),
+          ),
+        ],
       ),
     );
   }
--- lib/view/screens/FollowingScreen/following_screen.dart
+++ lib/view/screens/FollowingScreen/following_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:utopia/constants/color_constants.dart';
 import 'package:utopia/models/user_model.dart';
+import 'package:utopia/view/screens/ExploreScreen/components/search_box.dart';
 
 class FollowingScreen extends StatelessWidget {
   final User user;
@@ -14,6 +15,52 @@ class FollowingScreen extends StatelessWidget {
         title: const Text('Following',style: TextStyle(fontFamily: "Fira",fontSize: 15),),
         iconTheme: const IconThemeData(color: Colors.white),
       ),
-    );;
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: SearchBox(),
+          ),
+          Expanded(
+            child: ListView.builder(
+                  padding: EdgeInsets.all(8),
+                  itemCount: 15,
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemBuilder: (BuildContext, context) 
+                  {
+                  return Container(
+                    height: 70,
+                    width: 100,
+                    color: Colors.white60,
+                    margin:  EdgeInsets.all(8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children:   [
+                       Padding(
+                          padding: EdgeInsets.only(left: 6),
+                          child: CircleAvatar(
+                            radius: 28,
+                            backgroundColor: Colors.amber,
+                          ),
+                        ),
+                        Text('Kakashi Hatake', style: TextStyle(fontWeight: FontWeight.w700,fontSize: 17.5),),
+                        MaterialButton(onPressed: () {
+                          
+                        },
+                        color: authMaterialButtonColor,
+                        shape:  RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(6),
+                               ),
+                          child: Text('Following',style: TextStyle(fontSize: 15.5),),
+                        ),
+                      ],
+                    ),
+                  );
+                },),
+          ),
+        ],
+      ),
+    );
   }
 }
