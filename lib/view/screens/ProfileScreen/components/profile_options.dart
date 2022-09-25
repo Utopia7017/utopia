0e4c44e440df4c3a1fe012f09a7fe2ce0ce687fb
Subhojeet Sahoo <alphaisgod1@gmail.com>
2022-09-25 17:38:44 +0530

    local changes

--- lib/view/screens/ProfileScreen/components/profile_options.dart
+++ lib/view/screens/ProfileScreen/components/profile_options.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:utopia/view/screens/MyArticlesScreen/My_articles_screen.dart';
+import '../../MyArticlesScreen/my_articles_screen.dart';
 
 class ProfileOptions extends StatelessWidget {
   Widget tile(String label, Icon icon, Function() callback) {
