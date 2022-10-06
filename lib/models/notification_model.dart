44da4cc8060c8840d6f4a4a8f445536b418e264a
Subhojeet Sahoo <alphaisgod1@gmail.com>
2022-10-06 13:06:28 +0530

    update model

--- lib/models/notification_model.dart
+++ lib/models/notification_model.dart
@@ -1,16 +1,31 @@
 class Notification {
-  String type;
-  DateTime createdOn;
-  String authentication;
-  String update;
-  String deleteArticle;
+  final String notificationId;
+  final String type; // [ like,comment,follow ]
+  final DateTime createdOn;
+  final String notifierId;
+  String? articleId;
+  String? comment;
+  bool read;
 
   Notification({
+    required this.notificationId,
+    required this.notifierId,
     required this.type,
+    this.comment,
     required this.createdOn,
-    required this.authentication,
-    required this.update,
-    required this.deleteArticle,
-    
+    this.articleId,
+    required this.read,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'notificationId': '',
+      'comment': comment,
+      'read': false,
+      'createdOn': createdOn,
+      'notifierId': notifierId,
+      'type': type,
+      'articleId': articleId,
+    };
+  }
 }
