976444a96db243412b247c260685bf93d3d0641e
Subhojeet Sahoo <alphaisgod1@gmail.com>
2022-10-10 16:31:27 +0530

    made article screen's text selectable

--- lib/view/common_ui/article_textfield.dart
+++ lib/view/common_ui/article_textfield.dart
@@ -2,18 +2,34 @@ import 'package:flutter/material.dart';
 
 class ArticleTextField extends StatelessWidget {
   final TextEditingController controller;
-  ArticleTextField({required this.controller});
+  final bool isFirstTextBox;
+  ArticleTextField({required this.controller, required this.isFirstTextBox});
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      maxLines: null,
-      minLines: null,
-      showCursor: true,
-      cursorColor: Colors.black,
-      controller: controller,
-      expands: false,
-      decoration: const InputDecoration(border: InputBorder.none),
-    );
+    return isFirstTextBox
+        ? TextFormField(
+            maxLines: null,
+            minLines: null,
+            showCursor: true,
+            cursorColor: Colors.black,
+            controller: controller,
+            expands: false,
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+              hintText: "Start writing...",
+              hintStyle: TextStyle(color: Colors.black54, fontSize: 15),
+            ),
+          )
+        : TextFormField(
+            maxLines: null,
+            minLines: null,
+            showCursor: true,
+            cursorColor: Colors.black,
+            controller: controller,
+            expands: false,
+            decoration: const InputDecoration(
+                border: InputBorder.none, hintText: "Write here"),
+          );
   }
 }
