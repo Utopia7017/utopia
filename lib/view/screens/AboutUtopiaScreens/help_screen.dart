f6cad0973812de00aa0019259686c26607b0b59f
Subhojeet Sahoo <alphaisgod1@gmail.com>
2022-11-05 16:04:48 +0530

    added github option in help screen

--- lib/view/screens/AboutUtopiaScreens/help_screen.dart
+++ lib/view/screens/AboutUtopiaScreens/help_screen.dart
@@ -1,144 +1,238 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:quickalert/quickalert.dart';
 import 'package:utopia/constants/color_constants.dart';
 
-class HelpScreen extends StatelessWidget {
-  const HelpScreen({super.key});
+class HelpScreen extends StatefulWidget {
+  HelpScreen({super.key});
+
+  @override
+  State<HelpScreen> createState() => _HelpScreenState();
+}
+
+class _HelpScreenState extends State<HelpScreen> {
+  TextEditingController messageController = TextEditingController();
+
+  String type = "Feedback";
+
+  suggestFeebback(
+    String emailId,
+  ) async {
+    await FirebaseFirestore.instance
+        .collection('feedbacks')
+        .doc(emailId)
+        .collection('feedbacks')
+        .add({
+      'message': messageController.text,
+      'email': emailId,
+      'type': type,
+    });
+    QuickAlert.show(
+        context: context,
+        type: QuickAlertType.success,
+        title: "Successfully submitted feedback");
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 2,
-          backgroundColor: authBackground,
-          iconTheme: const IconThemeData(color: Colors.white),
-          title: const Text(
-            "Help & Support",
-            style: TextStyle(fontFamily: "Open", fontSize: 14),
+        backgroundColor: authBackground,
+        iconTheme: const IconThemeData(color: Colors.white),
+        title: const Text(
+          "Help & Support",
+          style: TextStyle(fontFamily: "Open", fontSize: 15),
+        ),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 20),
+            child: Icon(Icons.live_help_outlined),
           ),
-          actions: const [
-            Padding(
-              padding:  EdgeInsets.only(right: 20),
-              child: Icon(Icons.live_help_outlined),
-            ),
-          ],
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(30, 15, 10, 10),
-              child: Container(
-                child:  const Text('Reach us',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold,fontFamily: "open"),),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(30, 15, 10, 10),
+              child: Text(
+                'Reach us',
+                style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "open"),
               ),
             ),
-            const Divider(),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.0),
+              child: Text(
+                "Our app is open source and support reproducible builds. This means that anyone can independently verify that our code on Github is the exact same code that was used to build the app you download from Play Store. Developers are welcome to check out and contribute to our app on Github.",
+                style: TextStyle(fontFamily: "Open", fontSize: 14.5),
+              ),
+            ),
+            const Divider(
+              height: 15,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                CircleAvatar(
-                  backgroundColor: authBackground,
-                  child: IconButton(onPressed:  () {
-                    
-                  }, icon: const Icon(Icons.discord,color: Colors.white,)),
+                Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: authBackground,
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.discord,
+                            color: Colors.white,
+                          )),
+                    ),
+                    const SizedBox(height: 5),
+                    const Text(
+                      'Discord',
+                      style: TextStyle(fontFamily: "Open"),
+                    ),
+                  ],
                 ),
-                CircleAvatar(
-                  backgroundColor: authBackground,
-                  child: IconButton(onPressed: () {
-                    
-                  }, icon: const Icon(Icons.facebook,color: Colors.white,)),
+                Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: authBackground,
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.facebook,
+                            color: Colors.white,
+                          )),
+                    ),
+                    const SizedBox(height: 5),
+                    const Text(
+                      'Facebook',
+                      style: TextStyle(fontFamily: "Open"),
+                    ),
+                  ],
                 ),
-                CircleAvatar(
-                  backgroundColor: authBackground,
-                  child: IconButton(onPressed: () {
-                    
-                  }, icon: const Icon(Icons.email,color: Colors.white,)),
+                Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: authBackground,
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.email,
+                            color: Colors.white,
+                          )),
+                    ),
+                    const SizedBox(height: 5),
+                    const Text(
+                      'Email',
+                      style: TextStyle(fontFamily: "Open"),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: authBackground,
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            FontAwesomeIcons.github,
+                            color: Colors.white,
+                          )),
+                    ),
+                    const SizedBox(height: 5),
+                    const Text(
+                      'Github',
+                      style: TextStyle(fontFamily: "Open"),
+                    ),
+                  ],
                 ),
               ],
             ),
-            const SizedBox(height: 5,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: const [
-                Text('Discord',style: TextStyle(fontFamily: "open"),),
-                Text('Facebook',style: TextStyle(fontFamily: "open"),),
-                Text('Email',style: TextStyle(fontFamily: "open"),)
-              ],
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-           const Padding(
-              padding:  EdgeInsets.only(left: 30),
-              child: Text('Write your feedbacks here:',style: TextStyle(fontSize: 18,fontFamily: "open"),),
-            ),
-            const SizedBox(height: 15,),
-            // Padding(
-            //   padding: const EdgeInsets.fromLTRB(30, 2, 30, 2),
-            //   child: TextFormField(
-            //     decoration: InputDecoration(
-            //       hintText: 'feedback',hintStyle: const TextStyle(fontFamily: "open"),
-            //       border: OutlineInputBorder(
-            //         borderRadius: BorderRadius.circular(8),  
-            //       ),
-            //     ),
-            //   ),
-            // ),
+            const SizedBox(height: 20),
             Padding(
               padding: const EdgeInsets.fromLTRB(30, 2, 30, 2),
               child: DropdownButtonFormField(
-                        isExpanded: true,
-                        value: "Feedback",
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderSide: const BorderSide(color: Colors.white54),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        hint: const Text("Feedback"),
-                 items: ['Feedback','Complaints',"Suggetions (Feature/Bug fixes)"].map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                 onChanged: (value) {
-                
-              },),
+                isExpanded: true,
+                value: type,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.white54),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                items: [
+                  'Feedback',
+                  'Complaints',
+                  "Suggetions (Features / Fix bugs)"
+                ].map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    type = value.toString();
+                  });
+                },
+              ),
             ),
-            const SizedBox(height: 25,),
+            const SizedBox(
+              height: 15,
+            ),
             Padding(
               padding: const EdgeInsets.fromLTRB(30, 2, 30, 2),
               child: TextFormField(
-                maxLines: 8,
+                controller: messageController,
+                maxLines: 6,
                 decoration: InputDecoration(
-                  hintText: 'Enter your message here',hintStyle: const TextStyle(fontFamily: "open"),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),  
-                  )
-                ),
+                    hintText: 'Enter your message here',
+                    hintStyle: const TextStyle(fontFamily: "open"),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    )),
               ),
             ),
-             const SizedBox(height: 20,),
+            const SizedBox(
+              height: 15,
+            ),
             Padding(
               padding: const EdgeInsets.fromLTRB(30, 2, 30, 2),
               child: TextFormField(
+                initialValue: FirebaseAuth.instance.currentUser!.email,
+                readOnly: true,
                 decoration: InputDecoration(
-                  hintText: 'Email',hintStyle: const TextStyle(fontFamily: "open"),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),  
-                  )
-                ),
+                    hintStyle: const TextStyle(fontFamily: "Open"),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    )),
               ),
             ),
-            const SizedBox(height: 15,),
+            const SizedBox(
+              height: 15,
+            ),
             Center(
               child: MaterialButton(
-                child:  const Text('Send Feedback',style: TextStyle(fontFamily: "open"),),
                 color: authMaterialButtonColor,
-                onPressed: () {
-                
-              },),
+                onPressed: () async {
+                  await suggestFeebback(
+                      FirebaseAuth.instance.currentUser!.email!);
+                },
+                child: const Text(
+                  'Submit Feedback',
+                  style: TextStyle(
+                      fontFamily: "Open", fontWeight: FontWeight.w500),
+                ),
+              ),
             )
           ],
         ),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,6 +13,7 @@ import firebase_core
 import firebase_storage
 import path_provider_macos
 import sqflite
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -23,4 +24,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
