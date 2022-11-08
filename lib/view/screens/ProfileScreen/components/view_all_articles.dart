4967044afaf7ce1ab226d5dd6dbaaafd850ad95f
kaizer111 <mahatovishal2001@gmail.com>
2022-11-08 18:48:47 +0530

    View all screen for viewing articles>10

---
+++ lib/view/screens/ProfileScreen/components/view_all_articles.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:utopia/constants/color_constants.dart';
+import 'package:utopia/controller/my_articles_controller.dart';
+import 'package:utopia/view/common_ui/article_box.dart';
+
+
+class ViewAllArticles extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('All articles',style: TextStyle(fontFamily: "open"),),
+        backgroundColor: authBackground,
+        leading: IconButton(onPressed: () {
+          Navigator.pop(context);
+        }, icon: const Icon(Icons.arrow_back)),
+      ),
+      body: Consumer<MyArticlesController>(
+        builder: (context, myArticlesController, child) {
+          return ListView.builder(
+          itemCount: myArticlesController.publishedArticles.length,
+          itemBuilder: (context, index) {
+          return ArticleBox(article: myArticlesController.publishedArticles[index]);
+        },);
+        }, 
+      ),
+    );
+  }
+}
