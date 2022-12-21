414cdcccb94a9b564cd2951111bb9f74a3c56c70
kaizer111 <mahatovishal2001@gmail.com>
2022-12-21 19:01:33 +0530

    #236 implemented in app update

--- lib/view/screens/SplashScreen/splash_screen.dart
+++ lib/view/screens/SplashScreen/splash_screen.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:utopia/constants/color_constants.dart';
 import 'package:utopia/constants/image_constants.dart';
 import 'package:utopia/view/screens/UtopiaRoot/utopia_root.dart';
+import 'package:in_app_update/in_app_update.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -15,7 +17,34 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    navigateToHome();
+    checkForUpdate();
+  }
+
+  Future<void> checkForUpdate() async {
+
+    InAppUpdate.checkForUpdate().then((info) async {
+      // print(info.packageName);
+      // print(info.availableVersionCode);
+      
+      if (info.updateAvailability == UpdateAvailability.updateAvailable) {
+        AppUpdateResult updateResult =await InAppUpdate.performImmediateUpdate();
+        
+         if(updateResult==AppUpdateResult.userDeniedUpdate || updateResult == AppUpdateResult.inAppUpdateFailed) {
+            await Future.delayed(const Duration(seconds: 2));
+            checkForUpdate();
+          }
+          else{
+            navigateToHome();
+          }
+      } else {
+        navigateToHome();
+      }
+    }).catchError((e) {
+      if (kDebugMode) {
+        print("Update check error: " + e.toString());
+      }
+      navigateToHome();
+    });
   }
 
   navigateToHome() async {
